672bff16af6a24262b2827d774f24c280fc8e26b
Ranjitrajak <102968204+Ranjitrajak@users.noreply.github.com>
2022-08-07 20:52:01 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -15,6 +15,6 @@ mongoose.connect("mongodb+srv://root:Monkey_db%40123@cluster0.in2io.mongodb.net/
 
 app.use('/', route);
 
-app.listen(process.env.PORT || 3000, function () {
-    console.log('Express app running on port ' + (process.env.PORT || 3000))
+app.listen( 3000, function () {
+    console.log('Express app running on port ' + ( 3000))
 });
